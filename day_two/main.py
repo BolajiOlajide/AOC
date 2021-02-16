dee951bb0143bdeaca3d031337745fa32aa27554
Bolaji Olajide <cooproton@gmail.com>
2021-02-17 00:15:48 +0100

    complete day 2

---
+++ day_two/main.py
@@ -0,0 +1,42 @@
+# https://adventofcode.com/2020/day/2
+import itertools
+
+
+def part1(data):
+    count = 0
+
+    for [_range, _character, text] in data:
+        [lowerBound, upperBound] = list(map(int, _range.split('-')))
+        character = _character[0]
+
+        if (text.count(character) >= lowerBound) and (text.count(character) <= upperBound):
+            count += 1
+
+    return count
+
+
+def part2(data):
+    count = 0
+
+    for [_range, _character, text] in data:
+        [lowerBound, upperBound] = list(map(int, _range.split('-')))
+        character = _character[0]
+
+        first_index_correct = text[lowerBound - 1] == character
+        second_index_correct = text[upperBound - 1] == character
+
+        if first_index_correct != second_index_correct:
+            count += 1
+
+    return count
+
+
+def fetch_data_from_source():
+    with open('day_two/data.txt', 'r') as file:
+        return list(map(lambda x: x.rstrip().split(' '), file))
+
+if __name__ == '__main__':
+    data = fetch_data_from_source()
+
+    print('Part One: ', part1(data))
+    print('Part Two: ', part2(data))
